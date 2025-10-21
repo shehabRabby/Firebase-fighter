eb6759acd573723d423c1e2502b4cf1e4c6398f8
Md.Shehab Al Rabby <shehabrabby764@gmail.com>
2025-10-22 01:09:12 +0600

    Password hide show function applies

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router";
 import { auth } from "../firebase.config";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { toast } from "react-toastify";
+import { FaEye } from "react-icons/fa";
+import { IoEyeOff } from "react-icons/io5";
 
 const SignUp = () => {
+
+  const [show,setShow] = useState(false); //state for hide password 
+
+
+
   
   const handleSignup=(e)=>{
     e.preventDefault();
@@ -12,6 +19,14 @@ const SignUp = () => {
     const password = e.target.password.value;
     console.log("Signup function enter :",email,password );
 
+    //password validation use regex
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+    if(!passwordRegex.test(password)){
+      toast.error("Password must be at least 6 characters long and include uppercase, lowercase, number, and special character.");
+      return;
+    }
+      
+    //create user here 
     createUserWithEmailAndPassword(auth,email,password)
     .then((res) => {
       console.log(res);
@@ -52,15 +67,26 @@ const SignUp = () => {
          
          {/* from  */}
           <form onSubmit={handleSignup} className="flex flex-col gap-4">
+
             <div>{/* email  */}
-              <input type="email" name="email" placeholder="Email" className="w-full p-3 rounded-md bg-white text-black focus:outline-none"/>
+              <label htmlFor="">Email</label>
+              <input type="email" name="email" placeholder="Enter email here" className="w-full p-3 rounded-md bg-white text-gray-400  focus:outline-none"/>
             </div>
-            <div>{/* password  */}
-              <input type="password" name="password" placeholder="Password" className="w-full p-3 rounded-md bg-white text-black focus:outline-none"/>
+
+
+            <div className="relative">{/* password  */}
+              <label htmlFor="">Password</label>
+              <input type={show ? "text":"password"} name="password" placeholder="Enter Password" className="w-full p-3 rounded-md bg-white text-gray-400 focus:outline-none"/>
+              {/* eye icon*/}    
+              <span onClick={()=>{setShow(!show)}} className="absolute right-[8px] top-[40px] cursor-pointer">{show ? <FaEye></FaEye> : <IoEyeOff></IoEyeOff>}</span>
             </div>
+
+
             <div className="text-right">{/*Forget password  */}
               <a href="#" className="text-white text-sm hover:underline opacity-90">Forgot password?</a>
-            </div>{/* signUp Button */}
+            </div>
+            
+            {/* signUp Button */}
             <button type="submit" className="w-full py-3 bg-gradient-to-r from-pink-600 to-purple-600 text-white font-semibold rounded-md hover:opacity-90 transition">
               Sign Up
             </button>
